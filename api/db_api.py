04e6bfbdb45f6611526f1e4082ef7c7e30468b49
fexd12 <felipe.demelo@hotmail.com>
2020-04-23 19:30:53 -0300

    add remote db

--- api/db_api.py
+++ api/db_api.py
@@ -1,8 +1,6 @@
 import flask
 from flask import request, jsonify
-import sqlite3
-from sqlite3 import Error
-import pymysql.cursors
+import psycopg2
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
@@ -14,29 +12,26 @@ database = r"api/test.db"
 
 def create_connection():
     c = None
-    try:
-        c = sqlite3.connect(database)
-        print(sqlite3.version)
-        return c
-    except Error as e:
-        print(e)
+    con_string = "dbname=pi_v user=postgres password=1234"
+    
+    c = psycopg2.connect(con_string)
+    return c
 
+# def create_table(conn, create_table_sql):
+#     try:
+#         c = conn.cursor()
+#         c.execute(create_table_sql)
+#     except Error as e:
+#         print(e)
+#     finally:
+#         if c:
+#             c.close()
 
-def create_table(conn, create_table_sql):
-    try:
-        c = conn.cursor()
-        c.execute(create_table_sql)
-    except Error as e:
-        print(e)
-    finally:
-        if c:
-            c.close()
-
-def dict_factory(cursor, row):
-    d = {}
-    for idx, col in enumerate(cursor.description):
-        d[col[0]] = row[idx]
-    return d
+# def dict_factory(cursor, row):
+#     d = {}
+#     for idx, col in enumerate(cursor.description):
+#         d[col[0]] = row[idx]
+#     return d
 
 
 def execute(sql, isSelect=True):
@@ -44,28 +39,23 @@ def execute(sql, isSelect=True):
     # conn = sqlite3.connect(database)
     
     # REMOTE DATABASE
-    conn = pymysql.connect(host='remotemysql.com',
-                            port=3306,
-                            user='<USER>',
-                            password='<PASSWORD>',
-                            db='<DATABASE>',
-                            charset='utf8mb4',
-                            cursorclass=pymysql.cursors.DictCursor)
+    conn = create_connection()
     result = None
     try:
         with conn.cursor() as cursor:
             if isSelect:
                 cursor.execute(sql)
                 result = cursor.fetchall()
-                print(f"result = {result}")
+                # print(f"result = {result}")
             else:
                 cursor.execute(sql)
                 result = conn.insert_id()
                 conn.commit()
+    except Exception as exc:
+        print(exc)
     finally:
         conn.close()
     return result
-
 
 def start_db():
 
@@ -73,17 +63,21 @@ def start_db():
     conn = create_connection()
 
     # create tables
-    if conn is not None:
-        user_table_sql = """ CREATE TABLE IF NOT EXISTS user (
-                            id integer PRIMARY KEY AUTOINCREMENT,
-                            age integer NOT NULL,
-                            name text NOT NULL
-                        ); """
-        create_table(conn, user_table_sql)
-    else:
-        print("Error! cannot create the database connection.")
+    # if conn is not None:
+    #     user_table_sql = """ CREATE TABLE IF NOT EXISTS user (
+    #                         id integer PRIMARY KEY AUTOINCREMENT,
+    #                         age integer NOT NULL,
+    #                         name text NOT NULL
+    #                     ); """
+    #     create_table(conn, user_table_sql)
+    # else:
+    #     print("Error! cannot create the database connection.")
 
 # HTTP functions
+
+@app.route('/',methods=['GET'])
+def get():
+    return jsonify('hello World!')
 
 # Add new user
 @app.route('/user', methods=['POST'])
@@ -114,13 +108,10 @@ def put_users():
 def get_users():
 
     _id = request.args['id'] if 'id' in request.args else 0
-    _age = request.args['age'] if 'age' in request.args else 0
+    _age = request.args['email'] if 'email' in request.args else 0
     _name = request.args['name'] if 'name' in request.args else ''
 
-    sql = f"""SELECT * FROM user WHERE 
-            ({_id} = 0 OR id = {_id}) 
-            AND ({_age} = 0 OR age = {_age}) 
-            AND ('{_name}' = '' OR UPPER(name) = UPPER('{_name}'));"""
+    sql = f"""SELECT * FROM users"""
 
     users = execute(sql)
     return jsonify(users)
@@ -133,6 +124,5 @@ def delete_users(_id):
     return {}
 
 # LOCAL DATABASE
-# start_db()
 
-app.run()
+app.run(port=2020)
